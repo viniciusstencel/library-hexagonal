fef32059f0038cc860569e7753844f20815f5284
viniciusstencel <stencel.vinicius@gmail.com>
2026-09-18 18:31:53 -0300

    feat: Criando classe book e sua entity

---
+++ src/main/java/com/vinicius/library/adapters/out/persistence/entity/BookEntity.java
@@ -0,0 +1,50 @@
+package com.vinicius.library.adapters.out.persistence.entity;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "book")
+public class BookEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID uuid;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "year_of_publishment")
+    private Integer yearOfPublishment;
+
+
+    public BookEntity() {
+    }
+
+    public BookEntity(UUID uuid, String name, Integer yearOfPublishment) {
+        this.uuid = uuid;
+        this.name = name;
+        this.yearOfPublishment = yearOfPublishment;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getYearOfPublishment() {
+        return yearOfPublishment;
+    }
+
+    public void setYearOfPublishment(Integer yearOfPublishment) {
+        this.yearOfPublishment = yearOfPublishment;
+    }
+}
---
+++ src/main/java/com/vinicius/library/domain/model/Book.java
@@ -0,0 +1,40 @@
+package com.vinicius.library.domain.model;
+
+import java.util.UUID;
+
+public class Book {
+
+    private UUID id;
+    private String title;
+    private Integer yearOfPublishment;
+
+    public Book(UUID id, String title, Integer yearOfPublishment) {
+        this.id = id;
+        this.title = title;
+        this.yearOfPublishment = yearOfPublishment;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getYearOfPublishment() {
+        return yearOfPublishment;
+    }
+
+    public void setYearOfPublishment(Integer yearOfPublishment) {
+        this.yearOfPublishment = yearOfPublishment;
+    }
+}
